bafdd2a6c24babb83efc36242210ffb45f52a3bb
TheNotary <no@email.plz>
2023-08-27 20:01:21 -0500

    adding classes

--- int_to_pixels/int_to_pixels.py
+++ int_to_pixels/int_to_pixels.py
@@ -1,56 +1,52 @@
 import sys
 import tensorflow as tf
 from keras.models import Sequential, load_model
-from keras.layers import Dense, Embedding, Flatten, Reshape
+from keras.layers import Dense, Embedding, Flatten, Reshape, Lambda, Input
 import numpy as np
-print("TensorFlow version:", tf.__version__)
+from training_data import TrainingData
+# print("TensorFlow version:", tf.__version__)
 
-data = {
-    0: [[1, 1, 1], [1, 0, 1], [1, 0, 1], [1, 0, 1], [1, 1, 1]],
-    1: [[0, 0, 1], [0, 0, 1], [0, 0, 1], [0, 0, 1], [0, 0, 1]],
-    2: [[1, 1, 1], [0, 0, 1], [1, 1, 1], [1, 0, 0], [1, 1, 1]],
-    3: [[1, 1, 1], [0, 0, 1], [1, 1, 1], [0, 0, 1], [1, 1, 1]],
-    4: [[1, 0, 1], [1, 0, 1], [1, 1, 1], [0, 0, 1], [0, 0, 1]],
-    5: [[1, 1, 1], [1, 0, 0], [1, 1, 1], [0, 0, 1], [1, 1, 1]],
-    6: [[1, 1, 1], [1, 0, 0], [1, 1, 1], [1, 0, 1], [1, 1, 1]],
-    7: [[1, 1, 1], [0, 0, 1], [0, 0, 1], [0, 0, 1], [0, 0, 1]],
-    8: [[1, 1, 1], [1, 0, 1], [1, 1, 1], [1, 0, 1], [1, 1, 1]],
-    9: [[1, 1, 1], [1, 0, 1], [1, 1, 1], [0, 0, 1], [1, 1, 1]]
-}
+
+def main():
+    # check the cmd line args to see if they want to just train the model non-interactively
+    if len(sys.argv) > 1:
+        if sys.argv[1] == '--train':
+            train_model()
+            sys.exit(0)
+        elif sys.argv[1] == '--test':
+            run_tests()
+    
+    print("Choose mode:")
+    print("1 for training the model")
+    print("2 REPLing with the model")
+    print("3 for testing the model")
+    print("4 for model summary")
+    print("5 for targetted training_charge")
+    print("6 for complete training_charge")
+    mode = int(input())
+    if mode == 1:
+        train_model()
+    elif mode == 2:
+        repl_with_model()
+    elif mode == 3:
+        run_tests()
+    elif mode == 4:
+        summary()
+    elif mode == 5:
+        training_charge()
+    elif mode == 6:
+        training_charge_all()
+    else:
+        print("Invalid mode, exiting")
+
+
 
 def train_model():
-    X = np.array(list(data.keys()))
-    Y = np.array([np.array(data[key]).flatten() for key in data.keys()])
-
-    # 1. Input layer for a single byte
-    input_layer = tf.keras.layers.Input(shape=(1,), dtype=tf.uint8)
-    # 2. Normalize the byte to [0, 1]
-    normalized_input = tf.keras.layers.Lambda(lambda x: tf.cast(x, tf.float32) / 255.0)(input_layer)
-    # 3. Hidden layer (let's say with 32 units and ReLU activation)
-    hidden_layer = tf.keras.layers.Dense(15, activation='relu')(normalized_input)
-    hidden_layer = tf.keras.layers.Dense(32, activation='relu')(hidden_layer)
-    hidden_layer = tf.keras.layers.Dense(15, activation='relu')(hidden_layer)
-    # 4. Output layer with 15 parameters (let's assume a linear activation for simplicity)
-    output_layer = tf.keras.layers.Dense(15, activation='sigmoid')(hidden_layer)
-    # Construct the model
-    # model = Sequential([
-    #     input_layer,
-    #     normalized_input,
-    #     hidden_layer,
-    #     output_layer
-    # ])
-    model = tf.keras.Model(inputs=input_layer, outputs=output_layer)
-
-    # model = Sequential([
-    #     # Reshape(target_shape=(1,), input_shape=(1, 1)),
-    #     # Dense(1, input_shape=(1,)), # 1 input unit for the integer
-    #     Embedding(10, 10, input_length=1), # Embed the integers to a higher-dimensional space
-    #     Flatten(), # Flatten the embedding
-    #     # Dense(32, activation='relu'),
-    #     Dense(15, activation='sigmoid')  # 15 output units for the 3x5 grid
-    # ])
-    # model.add(tf.keras.layers.Dense(8, input_shape=(16,)))
-
+    model = build_original_model()
+    # model = build_uint8_model()
+    # model = build_one_hot_encoding_model()
+    
+    # Ok, show me the code for a network that uses one hot encoding to convert the input integer into a sparse representation then has a hidden layer before finally  having a 15 parameter output layer.
     model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
     
     # while True:
@@ -59,11 +55,48 @@ def train_model():
     #         break
     #     model.fit(X, Y, epochs=1000)
         
-    model.fit(X, Y, epochs=15000)
+    X, Y = TrainingData().get_training_inputs_outputs()
+    
+    model.fit(X, Y, epochs=1000)
         
     model.save('model.keras')
     print("Training done, now loading and using the model")
     model.summary()
+
+
+def build_uint8_model():
+    ######################
+    # Use uint8 as Input #
+    ######################
+    
+    # 1. Input layer for a single byte
+    input_layer = Input(shape=(1,), dtype=tf.uint8)
+    # 2. Normalize the byte to [0, 1]
+    normalized_input = Lambda(lambda x: tf.cast(x, tf.float32) / 255.0)(input_layer)
+    # 3. Hidden layer (let's say with 32 units and ReLU activation)
+    hidden_layer = Dense(15, activation='relu')(normalized_input)
+    hidden_layer = Dense(15, activation='relu')(hidden_layer)
+    hidden_layer = Dense(15, activation='relu')(hidden_layer)
+    hidden_layer = Dense(15, activation='relu')(hidden_layer)
+    hidden_layer = Dense(20, activation='relu')(hidden_layer)
+    # 4. Output layer with 15 parameters (let's assume a linear activation for simplicity)
+    output_layer = Dense(15, activation='sigmoid')(hidden_layer)
+    model = tf.keras.Model(inputs=input_layer, outputs=output_layer)
+    return model
+
+def build_original_model():
+    model = Sequential([
+        # Reshape(target_shape=(1,), input_shape=(1, 1)),
+        # Dense(1, input_shape=(1,)), # 1 input unit for the integer
+        Embedding(10, 10, input_length=1), # Embed the integers to a higher-dimensional space
+        Flatten(), # Flatten the embedding
+        # Dense(32, activation='relu'),
+        Dense(15, activation='sigmoid')  # 15 output units for the 3x5 grid
+    ])
+    return model
+
+def build_one_hot_encoding_model():
+    pass
 
 
 def repl_with_model():
@@ -98,13 +131,13 @@ def run_tests():
     failing_tests = []
     for i in range(10):
         grid = predict_pixels(i)
-        if grid == data[i]:
+        if grid == TrainingData.training_data[i]:
             passing_tests += 1
         else:
             failing_tests.append(i)
     
     assert passing_tests == 10, f"Failed {10 - passing_tests} tests: {failing_tests}"
-        # assert grid == data[i], f"Failed to predict {i} correctly\nExpected: \n{data[i]}\nGot: \n{grid}"
+        # assert grid == TrainingData.training_data[i], f"Failed to predict {i} correctly\nExpected: \n{training_data[i]}\nGot: \n{grid}"
     print("All tests passed!")
     sys.exit(0)
 
@@ -114,40 +147,25 @@ def summary():
 
 def training_charge():
     model = load_model('model.keras', safe_mode=False)
-    # X = np.array(list(data.keys()))
-    # Y = np.array([np.array(data[key]).flatten() for key in data.keys()])
+    # X = np.array(list(training_data.keys()))
+    # Y = np.array([np.array(training_data[key]).flatten() for key in training_data.keys()])
 
     X = np.array([8])
-    Y = np.array([np.array(data[8]).flatten()])
-    model.fit(X, Y, epochs=1000)
+    Y = np.array([np.array(TrainingData.training_data[8]).flatten()])
+    model.fit(X, Y, epochs=1)
     model.save('model.keras')
+    
+def training_charge_all():
+    model = load_model('model.keras', safe_mode=False)
+    X = np.array(list(TrainingData.training_data.keys()))
+    Y = np.array([np.array(TrainingData.training_data[key]).flatten() for key in TrainingData.training_data.keys()])
+
+    # X = np.array([8])
+    # Y = np.array([np.array(TrainingData.training_data[8]).flatten()])
+    model.fit(X, Y, epochs=9)
+    model.save('model.keras')
+    model
 
 
 if __name__ == '__main__':
-    # check the cmd line args to see if they want to just train the model non-interactively
-    if len(sys.argv) > 1:
-        if sys.argv[1] == '--train':
-            train_model()
-            sys.exit(0)
-        elif sys.argv[1] == '--test':
-            run_tests()
-    
-    print("Choose mode:")
-    print("1 for training the model")
-    print("2 REPLing with the model")
-    print("3 for testing the model")
-    print("4 for model summary")
-    print("5 for training_charge")
-    mode = int(input())
-    if mode == 1:
-        train_model()
-    elif mode == 2:
-        repl_with_model()
-    elif mode == 3:
-        run_tests()
-    elif mode == 4:
-        summary()
-    elif mode == 5:
-        training_charge()
-    else:
-        print("Invalid mode, exiting")
+    main()
